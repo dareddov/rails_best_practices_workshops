53ae60dab4f9101ed18eb7a6ff4ff292128feb28
lukasz <l3moniak2@gmail.com>
2014-04-26 12:17:05 +0200

    decent exposure for request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,55 +1,36 @@
 class RequestsController < ApplicationController
+  expose(:request_object, model: :request)
+  expose(:requests)
   before_action :authenticate_user!
   before_action :set_request, only: [:show, :edit, :update, :destroy]
 
-  # GET /requests
-  def index
-    @requests = Request.all
-  end
 
-  # GET /requests/1
-  def show
-  end
 
-  # GET /requests/new
-  def new
-    @request = Request.new
-  end
-
-  # GET /requests/1/edit
-  def edit
-  end
-
-  # POST /requests
   def create
-    @request = Request.new(request_params)
-
-    if @request.save
-      redirect_to @request, notice: 'Request was successfully created.'
+    if request.save
+      redirect_to request, notice: 'Request was successfully created.'
     else
-      render :new
+      render action: 'new'
     end
   end
 
-  # PATCH/PUT /requests/1
   def update
-    if @request.update(request_params)
-      redirect_to @request, notice: 'Request was successfully updated.'
+    if request.update(request_params)
+      redirect_to request, notice: 'Request was successfully updated.'
     else
-      render :edit
+      render action: 'edit'
     end
   end
 
-  # DELETE /requests/1
   def destroy
-    @request.destroy
+    request.destroy
     redirect_to requests_url, notice: 'Request was successfully destroyed.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_request
-      @request = Request.find(params[:id])
+      request = Request.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
